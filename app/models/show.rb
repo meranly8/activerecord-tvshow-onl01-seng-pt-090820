2a3c58c7f1640f4046339e6e20e82ab2cd5e5fd3
Michelle Ranly <meranly8@gmail.com>
2020-11-19 03:38:22 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -11,7 +11,7 @@ class Show < ActiveRecord::Base
     self.minimum(:rating)
   end
 
-  def self.least_popular
+  def self.least_popular_show
     self.where("rating = ?", self.lowest_rating).first
   end
 end
